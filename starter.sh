6201223d891051e9dbeed3907e92079465ddb952
reivenIV <runy.alen@gmail.com>
2022-10-27 14:32:51 +0100

    extra files

---
+++ starter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤╤"
+echo " Chose wich aplication you wanna run"
+echo "╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨╨"
+echo "► 1 ◄ For a simple calculator"
+echo "► 2 ◄ For an IP calculator"
+echo "► X ◄ To exit starter"
+echo "\n"
+echo -e "Your choise 1,2,X :  "
+read choise
+if [$choise == 1];
+then
+	python3 simpleCalcs.py
+
+elif [$choise == 2];
+then
+	python3 ipCalc.py
+		
+elif [$choise == X];
+then
+	exit
+	
+else 
+	echo "invalid input please try again" 
+fi
